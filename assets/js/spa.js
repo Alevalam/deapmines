6c6c68437f221b1f68fea615d729cd1fbe53e444
Alevalam <aav32014@mymail.pomona.edu>
2017-03-07 11:19:12 -0800

    Changed to work like efrain branch

--- assets/js/spa.js
+++ assets/js/spa.js
@@ -1,51 +1,86 @@
+// angular.module('Platzi', []);
+// angular.module('Platzi').controller('CanvasCtrl', ['$scope',
+// function CanvasCtrl($scope) {
+//     var canvas = document.getElementById('canvas');
+//     var context = canvas.getContext('2d');
+//
+//     $scope.data = [
+//
+//     ];
+//
+//     $scope.addData = function() {
+//         var id = 0;
+//         if($scope.data.length > 0) {
+//             id = $scope.data[$scope.data.length-1].id + 1;
+//         }
+//         var p = {id: id, x: $scope.x, y: $scope.y, height: $scope.height, width: $scope.width};
+//         $scope.data.push(p);
+//         $scope.x = '';
+//         $scope.y = '';
+//         $scope.height = '';
+//         $scope.width = '';
+//         draw($scope.data);
+//     };
+//
+//
+//     function draw(data) {
+//         for(var i=0; i<data.length; i++) {
+//             drawSquare(data[i]);
+//         }
+//     }
+//
+//     function drawSquare(data) {
+//         context.beginPath();
+//         context.fillRect(data.x, data.y, data.height, data.width);
+//     }
+//
+//     function isPointInsideRect(pointX,pointY,rectX,rectY,rectWidth,rectHeight){
+//     return  (rectX <= pointX) && (rectX + rectWidth >= pointX) &&
+//                  (rectY <= pointY) && (rectY + rectHeight >= pointY);
+//     }
+//
+//     // setup
+//     canvas.width = 600;
+//     canvas.height = 400;
+//     context.globalAlpha = 1.0;
+//     context.beginPath();
+//     draw($scope.data);
+// }]);
+//
+//
+//
+
 angular.module('Platzi', []);
-angular.module('Platzi').controller('CanvasCtrl', ['$scope', 
-function CanvasCtrl($scope) {
-    var canvas = document.getElementById('canvas');
-    var context = canvas.getContext('2d');
-    
-    $scope.data = [
-       
-    ];
-    
-    $scope.addData = function() {
-        var id = 0;
-        if($scope.data.length > 0) {
-            id = $scope.data[$scope.data.length-1].id + 1;
+angular.module('Platzi').controller('BaseCtrl', ['$scope',function ($scope) {
+
+    io.socket.get('/emoji', function (data) {
+      $scope.emojis = data;
+      $scope.$apply();
+    });
+
+    io.socket.on('emoji', function(event){
+        switch (event.verb) {
+            case 'created':
+                $scope.emojis.push(event.data);
+                $scope.$apply();
+                var ctx= canvas.getContext("2d");
+                var color = hash.concat(event.data.text);
+                ctx.fillStyle=color;
+                ctx.fillRect(0,0,canvas.width,canvas.height);
+                break;
         }
-        var p = {id: id, x: $scope.x, y: $scope.y, height: $scope.height, width: $scope.width};
-        $scope.data.push(p);
-        $scope.x = '';
-        $scope.y = '';
-        $scope.height = '';
-        $scope.width = '';
-        draw($scope.data);
-    };
-    
-    
-    function draw(data) {
-        for(var i=0; i<data.length; i++) {
-            drawSquare(data[i]);
-        }
-    }
-    
-    function drawSquare(data) {
-        context.beginPath();
-        context.fillRect(data.x, data.y, data.height, data.width);
-    }
+    });
 
-    function isPointInsideRect(pointX,pointY,rectX,rectY,rectWidth,rectHeight){
-    return  (rectX <= pointX) && (rectX + rectWidth >= pointX) &&
-                 (rectY <= pointY) && (rectY + rectHeight >= pointY);
-    }
+}]);
 
-    // setup
-    canvas.width = 600;
-    canvas.height = 400;
-    context.globalAlpha = 1.0;
-    context.beginPath();
-    draw($scope.data);
-}]);  
+var canvas = document.createElement("canvas");
+canvas.id     = "CursorLayer";
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+canvas.style.position = "absolute";
+canvas.style.zIndex = 8;
+canvas.style.border = "1ptx solid";
 
-
- 
+var hash = "#";
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(canvas);
